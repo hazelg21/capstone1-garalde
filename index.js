6c47c94397d2ca22f44320812d21587a0f577062
Hazel Garalde <hlgaralde21@gmail.com>
2022-02-07 19:03:12 +0800

    add value verification

--- index.js
+++ index.js
@@ -30,26 +30,59 @@ function sendMail(e) {
 }
 
 
+// sendBtn.addEventListener('click', function(e) {
+// 	sendMail(e)
+// })
+
+
+
+
 sendBtn.addEventListener('click', function(e) {
-	sendMail(e)
+    e.preventDefault()
+    if (document.getElementById('text-name').value !== '' && 
+        document.getElementById('text-email').value !== '' &&
+        document.getElementById('text-number').value !== '' &&
+        document.getElementById('text-textarea').value !== ''){
+
+         console.log("sending...")
+         sendMail(e)
+    
+    }
+    else if (document.getElementById('text-name').value === '') {
+        document.getElementById('req-name').innerHTML = 'required';
+    }
+    else if (document.getElementById('text-email').value === '') {
+        document.getElementById('req-email').innerHTML = 'required';
+    }
+    else if (document.getElementById('text-number').value === '') {
+        document.getElementById('req-number').innerHTML = 'required';
+    }
+    else if (document.getElementById('text-textarea').value === '') {
+        document.getElementById('req-textarea').innerHTML = 'required';
+    }
 })
 
-/*sendBtn.addEventListener('click', function(e) {
-    e.preventDefault()
-    fetch('https://powerful-shelf-02321.herokuapp.com/api/portfolio/contactMe', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                name: document.getElementById('text-name').value,
-                email: document.getElementById('text-email').value,
-                contactNo: document.getElementById('text-number').value,
-                msg: document.getElementById('text-textarea').value
-            })
-        })
-    .then(clearFields())
-    .then(togglePopup())
-})*/
 
+
+document.getElementById('text-name').addEventListener('keyup', function(e) {
+    e.preventDefault()
+    document.getElementById('req-name').innerHTML = '*';
+})
+
+document.getElementById('text-email').addEventListener('keyup', function(e) {
+    e.preventDefault()
+    document.getElementById('req-email').innerHTML = '*';
+})
+
+
+document.getElementById('text-number').addEventListener('keyup', function(e) {
+    e.preventDefault()
+    document.getElementById('req-number').innerHTML = '*';
+})
+
+
+document.getElementById('text-textarea').addEventListener('keyup', function(e) {
+    e.preventDefault()
+    document.getElementById('req-textarea').innerHTML = '*';
+})
 
